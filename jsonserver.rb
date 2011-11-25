e1c80f98e2a8743cf9cd81832c0425e082f4a9fc
Harald Hvaal <harald.hvaal@gmail.com>
2011-11-25 08:23:57 +0100

    added jsonserver

---
+++ jsonserver.rb
@@ -0,0 +1,70 @@
+require 'db.rb'
+require 'socket'
+require 'cgi'
+require 'json'
+
+class JsonServer
+
+    def initialize db, player, port
+        @db = db
+        @player = player
+        puts "Opening webserver at #{port}"
+        @server = TCPServer.open(port)
+
+        port = @server.addr[1]
+        addrs = @server.addr[2..-1].uniq
+        puts "*** initing on #{addrs.collect{|a|"#{a}:#{port}"}.join(' ')}"
+    end
+
+    def listen
+        while connection = @server.accept
+            headers = []
+            length  = 0
+            params  = nil
+
+            while line = connection.gets
+                headers << line
+
+                if line =~ /^Content-Length:\s+(\d+)/i
+                    length = $1.to_i
+                end
+
+                if line =~ /GET \/\?(.+) HTTP/i
+                    params = CGI::parse($1)
+                end
+
+                break if line == "\r\n"
+            end
+
+            connection.print "HTTP/1.1 200 OK\r\nContent-type: application/json\r\n\r\n"
+
+            if params
+                if params.include?("order")
+                    sid = params["order"].join(" ").to_i
+                    song = @db.lookup(sid)
+                    if song
+                        @player.queue(song)
+                    end
+                    connection.print"Ordered #{song.to_s}<br />\r\n"
+
+                elsif params.include?("terms")
+                    terms = params["terms"].join(" ")
+
+                    puts "Searching for " + terms
+                    songHashList = []
+                    myresults = @db.search(terms)
+                    myresults.each do |song|
+                        songHashList.push({"title" => song.to_s, "id" => song.sid.to_s })
+                    end
+                    result = { "songs" => songHashList }
+                    connection.puts(result.to_json)
+                end
+            end
+            connection.close
+        end
+
+    end
+
+end
+
+
--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'db.rb'
 require 'server.rb'
 require 'player.rb'
 require 'webserver.rb'
+require 'jsonserver.rb'
 
 require 'Qt'
 
@@ -10,19 +11,24 @@ app = Qt::Application.new(ARGV)
 
 db = Database.new
 #db.addFolder("/home/pvv/d/haraldhv/src/songs/")
-db.addFolder("/home/hhv/Karaoke/")
+#db.addFolder("/home/hhv/Karaoke/")
+db.addFolder("/home/harald/Karaoke/")
 db.sort!
 
 player = Player.new
 
-#Thread.new do
+jsonserver = JsonServer.new(db, player, 8888)
+jsonserver.listen
+
+
+Thread.new do
     webserver = WebServer.new(db, player, 7777)
     webserver.listen
-#end
+end
 
 Thread.new do
     server = Server.new(db, player, 5555)
     server.listen
 end
 
-player.play_queue
+#player.play_queue
